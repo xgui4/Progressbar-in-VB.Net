343f65a82db696cdcc7123a4ac8f9d690f196e1d
xgui4 <134389196+xgui4@users.noreply.github.com>
2024-07-13 09:34:20 -0400

    Add project files.

---
+++ VisualBasicApp/App.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class App
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PB = New ProgressBar()
+        MainLabel = New Label()
+        SuspendLayout()
+        ' 
+        ' PB
+        ' 
+        PB.BackColor = SystemColors.Control
+        PB.Location = New Point(12, 133)
+        PB.Name = "PB"
+        PB.Size = New Size(776, 185)
+        PB.TabIndex = 0
+        PB.Value = 5
+        ' 
+        ' MainLabel
+        ' 
+        MainLabel.AutoSize = True
+        MainLabel.Font = New Font("Segoe UI", 30.0F)
+        MainLabel.ForeColor = SystemColors.ControlLightLight
+        MainLabel.Location = New Point(120, 53)
+        MainLabel.Name = "MainLabel"
+        MainLabel.Size = New Size(515, 67)
+        MainLabel.TabIndex = 1
+        MainLabel.Text = "Progressbar in VB.NET"
+        ' 
+        ' App
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Desktop
+        ClientSize = New Size(800, 450)
+        Controls.Add(MainLabel)
+        Controls.Add(PB)
+        MaximizeBox = False
+        Name = "App"
+        SizeGripStyle = SizeGripStyle.Hide
+        Text = "Progressbar Visual Basic"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PB As ProgressBar
+    Friend WithEvents MainLabel As Label
+
+End Class
---
+++ VisualBasicApp/App.vb
@@ -0,0 +1,15 @@
+﻿Public Class App
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Hello There!")
+    End Sub
+
+    Private Sub PB_Click(sender As Object, e As EventArgs) Handles PB.Click
+        'Dim url As String = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        'If PB.Value = 99 Then Process.Start("explorer", url)
+        If PB.Value < 100 Then PB.Value = PB.Value + 1
+        If PB.Value = 100 Then
+            Console.Beep()
+            MessageBox.Show("The Progressbar is full")
+        End If
+    End Sub
+End Class
